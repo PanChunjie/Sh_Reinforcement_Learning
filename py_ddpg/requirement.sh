4018dc6d6a42b42d1d5078383598aa3464e687c3
Kratos <Kratos.X.Omega@gmail.com>
2019-06-12 17:32:04 -0400

    update requirement

--- py_ddpg/requirement.sh
+++ py_ddpg/requirement.sh
@@ -2,3 +2,4 @@
 # pip install pypiwin32
 # pip install tensorflow==1.13.0-rc1 #this version need to add "tensorflow." before keras import
 # pip install tensorflow==1.50 #this version need to remove "tensorflow." before keras import
+# pip keras==2.2.4
